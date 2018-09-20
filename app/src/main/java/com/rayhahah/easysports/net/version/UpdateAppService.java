b59334215519c23b5c9f92ecf9d9ebaa3aff6f5b
E <linxueyi2230@gmail.com>
2018-09-20 14:21:40 +0800

    sport

--- app/src/main/java/com/rayhahah/easysports/net/version/UpdateAppService.java
+++ app/src/main/java/com/rayhahah/easysports/net/version/UpdateAppService.java
@@ -7,7 +7,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.v7.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
+//import android.support.v7.app.NotificationCompat;
 
 import com.rayhahah.easysports.R;
 import com.rayhahah.easysports.utils.DialogUtil;
